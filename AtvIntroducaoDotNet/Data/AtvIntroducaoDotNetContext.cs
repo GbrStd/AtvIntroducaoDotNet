ac2f27501abebbf386016a69083f9d513271a24d
GbrStd <102706327+GbrStd@users.noreply.github.com>
2023-08-28 21:42:34 -0300

    Initial Commit

---
+++ AtvIntroducaoDotNet/Data/AtvIntroducaoDotNetContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AtvIntroducaoDotNet.Models;
+
+namespace AtvIntroducaoDotNet.Data
+{
+    public class AtvIntroducaoDotNetContext : DbContext
+    {
+        public AtvIntroducaoDotNetContext (DbContextOptions<AtvIntroducaoDotNetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AtvIntroducaoDotNet.Models.Equacao> Equacao { get; set; } = default!;
+    }
+}
---
+++ AtvIntroducaoDotNet/Models/Equacao.cs
@@ -0,0 +1,21 @@
+﻿namespace AtvIntroducaoDotNet.Models
+{
+    public class Equacao
+    {
+
+        public int Id { get; set; }
+
+        public float A { get; set; }
+
+        public float B { get; set; }
+
+        public float C { get; set; }
+
+        public float Delta { get; set; }
+
+        public float X1 { get; set; }
+
+        public float X2 { get; set; }
+
+    }
+}
